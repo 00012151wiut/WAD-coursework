a7ae19fc45e8af8891cb5b37944b956d30ae90ea
00012151wiut <123641097+00012151wiut@users.noreply.github.com>
2023-02-28 17:29:04 +0500

    Add files via upload

---
+++ Product_work_DRY.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WEB_Principles.Models
+{
+
+    class Program_work_DRY : Product_SRP
+    {
+        static void Main(string[] args)
+        {
+            DoSomething("1", "Coffee", "Cappucino");
+            DoSomething("2", "Coffee", "Americano");
+            DoSomething("3", "Cake", "Cheese teramisu");
+            DoSomething("4", "Cake", "Teramisu");
+            Console.ReadLine();
+        }
+        private static void DoSomething(string foodid, string foodtype, string foodname)
+        {
+            string FoodId = foodid;
+            string FoodType = foodtype;
+            string FoodName = foodname;
+            Console.WriteLine(FoodId, FoodType, FoodName);
+        }
+    }
+}
