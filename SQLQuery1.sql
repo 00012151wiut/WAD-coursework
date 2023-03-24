c81a435b9fe47b8e1360adbe4130a3799120327e
00012151wiut <123641097+00012151wiut@users.noreply.github.com>
2023-03-24 19:37:49 +0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE clients(
+id int not null primary key identity,
+name varchar(100) not null,
+email varchar (150) not null unique,
+phone varchar (20) null,
+address varchar(100) null,
+
+);
+
+
